20b893cbd420300810e628948c7b6ec4a1c3ea17
Anais Engelmajer <anaiscorinne@gmail.com>
2016-03-09 22:56:19 -0500

    Initial layout for homepage

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,22 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+$(window).bind('scroll', function() {
+	var distanceFromTop = $(this).scrollTop();
+	var headerHeight = $('.main-nav').height();
+		if (distanceFromTop >= headerHeight) {
+			$('.main-nav').addClass('nav-transparent')
+			$('.main-nav').removeClass('black')
+		}
+		else {
+			$('.main-nav').removeClass('nav-transparent')
+			$('.main-nav').addClass('black')
+		}
+	});
+
+  $('.slider').slider({full_width: true,
+  										 transition: 500,
+  										 height: 600,
+  										 indicators: false,
+  										 interval: 3500});
+});
